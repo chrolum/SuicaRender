46e777a4335bc961b1aa9b80bac9d0e10c71e63e
Saltsuica <740050093@qq.com>
2021-10-24 00:20:08 +0800

    first line

---
+++ lineDrawer.cpp
@@ -0,0 +1,11 @@
+#include "lineDrawer.h"
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    for (float t = 0.0f; t < 1; t += 0.01)
+    {
+        int x = x0 + (x1 - x0) * t;
+        int y = y0 + (y1 - y0) * t;
+        image.set(x, y, color);
+    }
+}
---
+++ lineDrawer.h
@@ -0,0 +1,6 @@
+#ifndef LINEDRAWER_H
+#define LINEDRAWER_H
+
+#include "tgaimage.h"
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+#endif //LINEDRAWER_H
